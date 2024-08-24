d47882ae7715f394048911e1711011c9a1903d7b
JoseM236 <116234062+JoseM236@users.noreply.github.com>
2024-08-24 13:14:54 -0300

    Update sistema_solar.c

--- sistema_solar.c
+++ sistema_solar.c
@@ -1,71 +1,105 @@
 #include <GL/glut.h>
 #include <SOIL/SOIL.h>
 
-// Tamanhos dos planetas e Sol
-float planetSizes[] = {0.383, 0.949, 1.0, 0.532, 11.21, 9.45, 4.01, 3.88, 0.186};
-float planetDistances[] = {5.8, 10.8, 15.0, 22.8, 77.8, 143.0, 287.0, 450.0, 590.0};
-GLuint textures[10];
+GLuint sunTexture, mercuryTexture, venusTexture, earthTexture, marsTexture;
+GLuint jupiterTexture, saturnTexture, uranusTexture, neptuneTexture;
 
 void loadTextures() {
-    const char* filenames[] = {
-        "sun.jpg", "mercury.jpg", "venus.jpg", "earth.jpg", 
-        "mars.jpg", "jupiter.jpg", "saturn.jpg", "uranus.jpg", 
-        "neptune.jpg", "pluto.jpg"
-    };
-    
-    glGenTextures(10, textures);
-
-    for (int i = 0; i < 10; i++) {
-        glBindTexture(GL_TEXTURE_2D, textures[i]);
-        int width, height;
-        unsigned char* image = SOIL_load_image(filenames[i], &width, &height, 0, SOIL_LOAD_RGB);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-        SOIL_free_image_data(image);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
+    sunTexture = SOIL_load_OGL_texture("sol.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    mercuryTexture = SOIL_load_OGL_texture("mercurio.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    venusTexture = SOIL_load_OGL_texture("venus.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    earthTexture = SOIL_load_OGL_texture("terra.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    marsTexture = SOIL_load_OGL_texture("marte.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    jupiterTexture = SOIL_load_OGL_texture("jupiter.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    saturnTexture = SOIL_load_OGL_texture("saturno.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    uranusTexture = SOIL_load_OGL_texture("urano.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    neptuneTexture = SOIL_load_OGL_texture("netuno.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
 }
 
-void drawSphere(float size, GLuint texture) {
-    glEnable(GL_TEXTURE_2D);
+void drawSphere(GLfloat radius, GLuint texture) {
     glBindTexture(GL_TEXTURE_2D, texture);
-    GLUquadric* quad = gluNewQuadric();
+    GLUquadric *quad = gluNewQuadric();
     gluQuadricTexture(quad, GL_TRUE);
-    gluSphere(quad, size, 50, 50);
+    gluSphere(quad, radius, 50, 50);
     gluDeleteQuadric(quad);
-    glDisable(GL_TEXTURE_2D);
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    gluLookAt(0.0, 0.0, 1000.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glEnable(GL_TEXTURE_2D);
 
-    // Desenha o Sol no centro
-    drawSphere(5.0, textures[0]);
+    // Render Sun
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, sunTexture);
+    drawSphere(2.0f, sunTexture);
+    glPopMatrix();
 
-    // Desenha os planetas
-    for (int i = 0; i < 9; i++) {
-        glPushMatrix();
-        glTranslatef(planetDistances[i], 0.0, 0.0);
-        drawSphere(planetSizes[i], textures[i + 1]);
-        glPopMatrix();
-    }
+    // Render Mercury
+    glPushMatrix();
+    glTranslatef(3.0f, 0.0f, 0.0f);
+    drawSphere(0.5f, mercuryTexture);
+    glPopMatrix();
 
+    // Render Venus
+    glPushMatrix();
+    glTranslatef(5.0f, 0.0f, 0.0f);
+    drawSphere(0.6f, venusTexture);
+    glPopMatrix();
+
+    // Render Earth
+    glPushMatrix();
+    glTranslatef(7.0f, 0.0f, 0.0f);
+    drawSphere(0.7f, earthTexture);
+    glPopMatrix();
+
+    // Render Mars
+    glPushMatrix();
+    glTranslatef(9.0f, 0.0f, 0.0f);
+    drawSphere(0.6f, marsTexture);
+    glPopMatrix();
+
+    // Render Jupiter
+    glPushMatrix();
+    glTranslatef(12.0f, 0.0f, 0.0f);
+    drawSphere(1.2f, jupiterTexture);
+    glPopMatrix();
+
+    // Render Saturn
+    glPushMatrix();
+    glTranslatef(15.0f, 0.0f, 0.0f);
+    drawSphere(1.0f, saturnTexture);
+    glPopMatrix();
+
+    // Render Uranus
+    glPushMatrix();
+    glTranslatef(18.0f, 0.0f, 0.0f);
+    drawSphere(0.8f, uranusTexture);
+    glPopMatrix();
+
+    // Render Neptune
+    glPushMatrix();
+    glTranslatef(21.0f, 0.0f, 0.0f);
+    drawSphere(0.8f, neptuneTexture);
+    glPopMatrix();
+
+    glDisable(GL_TEXTURE_2D);
     glutSwapBuffers();
 }
 
 void init() {
     glEnable(GL_DEPTH_TEST);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     loadTextures();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Solar System");
+    glutCreateWindow("Solar System Visualization");
+
     init();
+
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
