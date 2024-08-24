ac5ad84c1018d05d54fabee12e47bf19d05c5259
JoseM236 <116234062+JoseM236@users.noreply.github.com>
2024-08-24 19:27:40 -0300

    Update sistema_solar.c

--- sistema_solar.c
+++ sistema_solar.c
@@ -1,63 +1,63 @@
 #include <GL/glut.h>
-#include <SOIL/SOIL.h> // Biblioteca para carregar texturas
+#include <GL/glu.h>
+#include <SOIL/SOIL.h>
 
-GLuint textureSun;
-GLuint texturePlanet;
+GLuint texture;
 
-void loadTexture(const char* filename, GLuint* textureID) {
-    int width, height;
-    unsigned char* image = SOIL_load_image(filename, &width, &height, 0, SOIL_LOAD_RGB);
-    glGenTextures(1, textureID);
-    glBindTexture(GL_TEXTURE_2D, *textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+void loadTexture() {
+    texture = SOIL_load_OGL_texture(
+        "sol.jpg",  // caminho para a imagem da textura
+        SOIL_LOAD_AUTO,
+        SOIL_CREATE_NEW_ID,
+        SOIL_FLAG_INVERT_Y
+    );
+
+    if (!texture) {
+        printf("Erro ao carregar a textura: '%s'\n", SOIL_last_result());
+        exit(1);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Configurações de textura
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    SOIL_free_image_data(image);
 }
 
-void drawSphere(float radius, GLuint textureID) {
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glutWireSphere(radius, 20, 20); // Use glutSolidSphere se preferir uma esfera sólida
+void init() {
+    glEnable(GL_TEXTURE_2D);    // Ativar texturas
+    loadTexture();
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Configuração da câmera
-    gluLookAt(0.0, 0.0, 5.0,   0.0, 0.0, 0.0,   0.0, 1.0, 0.0);
+    // Posicionar a esfera
+    glTranslatef(0.0f, 0.0f, -5.0f);
 
-    // Desenha o Sol
-    glPushMatrix();
-    glTranslatef(0.0, 0.0, -2.0);
-    drawSphere(1.0, textureSun);
-    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, texture);
 
-    // Desenha o Planeta
-    glPushMatrix();
-    glTranslatef(2.0, 0.0, -3.0);
-    drawSphere(0.5, texturePlanet);
-    glPopMatrix();
+    // Criar a esfera e aplicar a textura
+    GLUquadric* quad = gluNewQuadric();
+    gluQuadricTexture(quad, GL_TRUE);
+    gluSphere(quad, 1.0, 50, 50);  // raio 1.0, subdivisões 50x50
+
+    gluDeleteQuadric(quad);
 
     glutSwapBuffers();
-}
-
-void init() {
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-
-    loadTexture("sol.jpg", &textureSun);
-    loadTexture("terra.jpg", &texturePlanet);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Sun and Planet");
+    glutCreateWindow("Textura do Sol em OpenGL");
 
     init();
+
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
